4fccc080e4b4d1e1f9c4d161c4ad87ef647de902
ankitmankar0 <ankitmankar012@gmail.com>
2022-04-01 15:22:04 +0530

    Retrive Salary

--- Payroll_Service.sql
+++ Payroll_Service.sql
@@ -21,7 +21,7 @@ CREATE TABLE employee_payroll
 
 --UC3 : Ability to create employee payroll data service database
 INSERT INTO employee_payroll (name,salary,startDate) VALUES
-('Bill', 100000.00, '2018-01-03'),
+('Lilly', 150000.00, '2017-01-03'),
 ('Terisa', 200000.00, '2019-11-13'),
 ('Charlie', 300000.00, '2020-05-21');
 Insert into employee_payroll (name,salary,startDate) values('Ankit',5545,GETDATE()),('Rohan',54545,GETDATE())
@@ -34,3 +34,19 @@ Insert into employee_payroll (name,salary,startDate) values('Ankit',5545,GETDATE
 --UC4 : Ability to retrieve all the employee payroll data 
 select * 
 from employee_payroll;
+
+
+--UC5 : Ability to retrieve salary data for a particular
+--      employee as well as all employees who have
+--      joined in a particular data range from the
+--      payroll service database
+--Use SELECT salary FROM employee_payroll WHERE name = 'Bill’ Query to View Bill’s salary.
+select salary from employee_payroll
+where name = 'Bill';                 --where condition use to get only salary of Bill
+
+select *  from employee_payroll
+where name = 'Bill';                 --where condition use to get all details of Bill
+
+--Use Select query with Where condition View employees between start date and now date.
+select * from employee_payroll
+where startDate BETWEEN CAST('2018-01-01' as DATE) AND GETDATE();
